a64d5e7275a38e7b02e1fd9521922b103b757c87
Jonathan Gallo <jonnygallo@gmail.com>
2018-09-12 16:43:07 -0700

    React intro challenge complete

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 
 import App from './components/app';
 
+
+
+const user  = {
+    firstName: 'John',
+    lastName: 'Smith'
+};
+
+function fullName (user){
+    return `${user.firstName} ${user.lastName}`;
+}
+
+const greeting = <h1>Hello {fullName(user)}!</h1>;
+
 ReactDOM.render(
-    <App />,
+    greeting,
     document.getElementById('root')
 );
