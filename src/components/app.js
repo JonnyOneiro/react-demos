c0df5d515ab214f26221b5b54e7c935022a5fac9
Jonathan Gallo <jonnygallo@gmail.com>
2018-09-14 16:06:30 -0700

    created branch component state challenge

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import '../assets/css/app.css';
 import logo from '../assets/images/logo.svg';
+import Greeting from './greeting';
 
 const App = () => (
     <div>
         <div className="app">
+            <Greeting name="Bobby" favColor="blue"/>
+            <Greeting name="Sally" favColor="hotpink"/>
             <img src={logo} className="logo rotate"/>
             <h1>Welcome to React</h1>
         </div>
